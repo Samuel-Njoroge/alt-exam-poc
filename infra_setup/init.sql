0ec0ea51d837ef6cc4f199c43a411dfa268c05fe
Samuel-Njoroge <njorogekiragu8@gmail.com>
2024-04-24 19:03:47 +0300

    set up

--- infra_setup/init.sql
+++ infra_setup/init.sql
@@ -20,11 +20,14 @@ FROM '/data/products.csv' DELIMITER ',' CSV HEADER;
 -- TODO: Provide the DDL statment to create this table ALT_SCHOOL.CUSTOMERS
 create table if not exists ALT_SCHOOL.CUSTOMERS
 (
-    
+ customer_id uuid not null primary key,
+ device_id uuid,
+ location varchar,
+ currency numeric(10, 2)
 );
 
 -- TODO: provide the command to copy the customers data in the /data folder into ALT_SCHOOL.CUSTOMERS
-COPY ALT_SCHOOL.CUSTOMERS ()
+COPY ALT_SCHOOL.CUSTOMERS (customer_id, device_id, location, currency)
 FROM '/data/customers.csv' DELIMITER ',' CSV HEADER;
 
 
@@ -32,33 +35,43 @@ FROM '/data/customers.csv' DELIMITER ',' CSV HEADER;
 create table if not exists ALT_SCHOOL.ORDERS
 (
     order_id uuid not null primary key,
+    customer_id uuid,
+    status varchar,
+    checked_out_at timestamp
     -- provide the other fields
 );
 
 
 -- provide the command to copy orders data into POSTGRES
-COPY ALT_SCHOOL.ORDERS ()
+COPY ALT_SCHOOL.ORDERS (order_id, customer_id, status, checked_out_at)
 FROM '/data/orders.csv' DELIMITER ',' CSV HEADER;
 
 create table if not exists ALT_SCHOOL.LINE_ITEMS
 (
     line_item_id serial primary key,
+    order_id uuid,
+    item_id numeric,
+    quantity numeric
     -- provide the remaining fields
 );
 
 
 -- provide the command to copy ALT_SCHOOL.LINE_ITEMS data into POSTGRES
-COPY ALT_SCHOOL.LINE_ITEMS ()
+COPY ALT_SCHOOL.LINE_ITEMS (line_item_id, order_id, item_id, quantity)
 FROM '/data/line_items.csv' DELIMITER ',' CSV HEADER;
 
--- setup the events table following the examle provided
+-- setup the events table following the example provided
 create table if not exists ALT_SCHOOL.EVENTS
 (
     -- TODO: PROVIDE THE FIELDS
+    event_id serial primary key,
+    customer_id uuid,
+    event_data jsonb,
+    event_timestamp timestamp
 );
 
 -- TODO: provide the command to copy ALT_SCHOOL.EVENTS data into POSTGRES
-COPY ALT_SCHOOL.EVENTS ()
+COPY ALT_SCHOOL.EVENTS (event_id, customer_id, event_data, event_timestamp)
 FROM '/data/events.csv' DELIMITER ',' CSV HEADER;
 
 
---
+++ questions/answers.sql
@@ -0,0 +1,43 @@
+-- Part 2a --
+--1. what is the most ordered item based on the number of times it appears in an order cart that checked out successfully? you are expected to return the product_id, and product_name and num_times_in_successful_orders where:
+
+-- product_id: the uuid string uniquely representing the product
+-- product_name: the name of the product as provided in the product table
+-- the number of times the product appeared in a a users cart that was successfully checked out. if ten customers added the item to their carts and only 8 of them successfully checked out and paid, then the answer should be 8 not 10.
+
+SELECT
+    product_id,
+    product_name,
+    num_times_in_successful_orders
+FROM 
+
+-- 2. Without considering currency, and without using the line_item table, find the top 5 spenders you are expected to return the customer_id, location, total_spend where:
+-- customer_id: uuid string that uniquely identifies a customer
+-- location - the customer's location
+-- total_spend - the total amount of money spent on orders
+
+
+-- Part 2b
+-- 1. Using the events table, Determine the most common location (country) where successful checkouts occurred. return location and checkout_count where:
+-- location: the name of the location
+-- checkout_count: the number of checkouts that occured in the location
+
+SELECT
+    location,
+    checkout_count
+FROM
+
+-- 2. Using the events table, identify the customers who abandoned their carts and count the number of events (excluding visits) that occurred before the abandonment. return the customer_id and num_events where:
+-- customer_id: id uniquely identifying the customers
+-- num_events: the number of events excluding visits that occured before abandonment
+
+SELECT
+    customer_id,
+    num_events
+FROM
+
+-- 3. Find the average number of visits per customer, considering only customers who completed a checkout! return average_visits to 2 decimal place
+-- average_visits: this number is a metric that suggests the avearge number of times a customer visits the website before they make a successful transaction!
+
+SELECT
+    AVG()
